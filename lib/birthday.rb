5b22dedf4dce9035885c9f5ad0bbe82859d114a6
Uriah Harston <uharston@gmail.com>
2020-05-11 17:04:47 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,9 +9,4 @@ def happy_birthday(birthday_kids)
   end
 end
 
-def age_appropriate_birthday(birthday_kids)
-  birthday_kids.each do |name, age|
-    if age < 13
-      puts "Happy Birthday #{name}! You are now #{age} years old!"
-    end
-  end
+
