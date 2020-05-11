5614488a42ec5b200a2255220435d3fa64bd4bd3
Uriah Harston <uharston@gmail.com>
2020-05-11 17:01:51 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,10 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  passengers.each do |suite, passenger|
+    if suite == :suite_a and passenger.start_with?("A")
+      winner = nmae
+    end
+  end
+  winner
 end
